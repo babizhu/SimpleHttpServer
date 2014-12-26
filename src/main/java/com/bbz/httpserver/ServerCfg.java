5e6090b6f8f0c90b0605a41bf2daea61d2cffdc7
liukun <liu_kun@msn.com>
2014-12-26 14:46:49 +0800

    解决了propties文件的中文字符集问题，确保该文件采用utf-8的方式保存

--- src/main/java/com/bbz/httpserver/ServerCfg.java
+++ src/main/java/com/bbz/httpserver/ServerCfg.java
@@ -1,17 +1,13 @@
 package com.bbz.httpserver;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Properties;
 
 /**
  * user         LIUKUN
  * time         2014-4-7 13:38
- *
+ * <p/>
  * 服务器的整体配置文件
- *
  */
 
 public class ServerCfg{
@@ -19,24 +15,37 @@ public class ServerCfg{
     /**
      * 监听ip
      */
-    public static final String          IP;
+    public static final String IP;
 
     /**
      * 监听端口
      */
-    public static final int             PORT;
+    public static final int PORT;
 
 
     /**
      * web的根目录
      */
-    public static final String          HOME_DIR;
+    public static final String HOME_DIR;
 
-    static {
+    static{
         Properties prop = new Properties();
+
+//            InputStream in = new BufferedInputStream( new FileInputStream( "resource/server.properties" ) );
+//            prop.load( in );
+//            in.close();
         try {
-            InputStream in = new BufferedInputStream( new FileInputStream( "resource/server.properties" ) );
-            prop.load( in );
+//            InputStream in = new BufferedInputStream( new FileInputStream( "resource/path.properties" ) );
+//            prop.load( in );
+//            in.close();
+            File file = new File( "resource/server.properties" );
+            FileInputStream inputStream = new FileInputStream( file );
+            InputStreamReader streamReader = new InputStreamReader( inputStream );
+            prop.load( streamReader );
+
+            inputStream.close();
+            streamReader.close();
+
 
         } catch( IOException e ) {
             e.printStackTrace();
@@ -48,7 +57,7 @@ public class ServerCfg{
     }
 
     public static void main( String[] args ){
-
+        System.out.println( ServerCfg.HOME_DIR);
     }
 }
 
