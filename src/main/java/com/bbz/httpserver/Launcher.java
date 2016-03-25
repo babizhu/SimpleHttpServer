6b9bfbec4a1ddc7eaf0b21cbbed6fbaa094427d8
liu_kun <liu_kun@msn.com>
2016-03-25 16:27:04 +0800

    修改了启动类的名字

--- src/main/java/com/bbz/httpserver/Launcher.java
+++ src/main/java/com/bbz/httpserver/Launcher.java
@@ -7,7 +7,7 @@ import com.bbz.httpserver.server.HttpStaticFileServer;
  * time         2014-12-26 12:39
  */
 
-public class Lancher{
+public class Launcher{
     public static void main( String[] args ) throws Exception{
 
         System.out.println( "HOME目录：" + ServerCfg.HOME_DIR);
